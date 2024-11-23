a1cb6a4894badc4507519d58bee5408f507c211d
mHsnzd <84739896+mHsnzd@users.noreply.github.com>
2024-11-23 15:23:18 -0800

    update sentiment analysis
    updated the sentiment analysis python scripts to match the new format of comments

--- sentiment_analysis_spark_vivekn.py
+++ sentiment_analysis_spark_vivekn.py
@@ -1,10 +1,11 @@
-"""
+'''
 This script performs sentiment analysis on the labeled comments using Spark NLP. A pretrained model based on the approach by Vivek Narayanan is used: https://sparknlp.org/2021/11/22/sentiment_vivekn_en.html
 
 To locally run the script use the command:
 spark-submit --packages com.johnsnowlabs.nlp:spark-nlp_2.12:5.5.1 sentiment_analysis_spark.py input-parquet-path output-parquet-path
 
-"""
+'''
+
 
 import sys
 assert sys.version_info >= (3, 5) # Make sure we have Python 3.5+
@@ -17,31 +18,40 @@ from pyspark.ml import Pipeline
 from pyspark.sql.functions import col
 
 
+# Constants:
+COMMENT_TXT_FIELD = 'body' 
+PARTY_LABEL_FIELD = 'label'
+PARTY_LABELS = ['conservative', 'liberal'] 
+SENTIMENT_LABEL_FIELD = 'sentiment'
+
+
 def main(input, output):
     # Read data from files
     comments_df = spark.read.parquet(input)
+    # Ignore neither or both labels
+    comments_df = comments_df.filter(comments_df[PARTY_LABEL_FIELD].isin(PARTY_LABELS))
 
-    # Convert the title_body column of the dataframe into the acceptable input for the model 
+    # Convert the text column of the dataframe into the acceptable input for the model 
     document = DocumentAssembler() \
-        .setInputCol("title_body") \
-        .setOutputCol("document")
+        .setInputCol(COMMENT_TXT_FIELD) \
+        .setOutputCol('document')
     
     token = Tokenizer() \
-        .setInputCols(["document"]) \
-        .setOutputCol("token")
+        .setInputCols(['document']) \
+        .setOutputCol('token')
 
     normalizer = Normalizer() \
-        .setInputCols(["token"]) \
-        .setOutputCol("normal")
+        .setInputCols(['token']) \
+        .setOutputCol('normal')
 
     # Use the pre-trained sentiment-analysis-vivekn model
     vivekn =  ViveknSentimentModel.pretrained() \
-        .setInputCols(["document", "normal"]) \
-        .setOutputCol("result_sentiment")
+        .setInputCols(['document', 'normal']) \
+        .setOutputCol('result_sentiment')
 
     finisher = Finisher() \
-        .setInputCols(["result_sentiment"]) \
-        .setOutputCols("sentiment")
+        .setInputCols(['result_sentiment']) \
+        .setOutputCols(SENTIMENT_LABEL_FIELD)
 
     # Build the pipeline from all the stages
     pipeline = Pipeline().setStages([document, token, normalizer, vivekn, finisher])
@@ -54,6 +64,7 @@ def main(input, output):
     # result.show(truncate=False)
     result.repartition(20).write.parquet(output, mode='overwrite')
 
+
 if __name__ == '__main__':
     # Reading the  command line arguments
     input = sys.argv[1] 
@@ -62,7 +73,7 @@ if __name__ == '__main__':
     # Initializing sparksession with necessary packages (Alternatively use spark = sparknlp.start())
     spark = SparkSession.builder \
         .appName('Sentiment Analysis Spark NLP') \
-        .config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp_2.12:5.5.1") \
+        .config('spark.jars.packages', 'com.johnsnowlabs.nlp:spark-nlp_2.12:5.5.1') \
         .getOrCreate()
     assert spark.version >= '3.0' # Make sure we have Spark 3.0+
     spark.sparkContext.setLogLevel('WARN')
