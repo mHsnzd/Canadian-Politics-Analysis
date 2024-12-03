ee8dbb6b1fb96105cdb7ae324a0f9bc34e130286
mHsnzd <84739896+mHsnzd@users.noreply.github.com>
2024-12-02 16:17:07 -0800

    Add hate speech detection
    Added two different methods of finding out whether a comment contains hate speech or not.

--- emoji_count/emoji_count.py
+++ emoji_count/emoji_count.py
@@ -31,7 +31,7 @@ def extract_emojis(comment):
                                u"\U0001F600-\U0001F64F"  # emoticons
                             #    u"\U0001F300-\U0001F5FF"  # symbols & pictographs
                                u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                            #    u"\U0001F1E6-\U0001F1FF"  # flags (iOS)
                                u"\U00002702-\U000027B0"  # Dingbats
                             #    u"\U000024C2-\U0001F251"
                                "]", flags=re.UNICODE)
@@ -66,7 +66,7 @@ def main(input, output):
 if __name__ == '__main__':
     # Reading the  command line arguments
     input = sys.argv[1] 
-    output = sys.argv[2]        
+    output = sys.argv[2]       
 
     # Initializing sparksession with necessary packages (Alternatively use spark = sparknlp.start())
     spark = SparkSession.Builder() \
